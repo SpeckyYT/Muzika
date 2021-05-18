d282b9294ab333daa3fda4f79ed4306d0f01d1a8
SpeckyYT <Specky.fans@gmail.com>
2021-05-18 22:32:34 +0200

    implemented loader in index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('colors').enable();
+
 const path = require('path');
 const { Client } = require('discord.js');
+const filehound = require('filehound');
 
 const client = new Client({
     intents: [
@@ -10,19 +12,32 @@ const client = new Client({
     ],
 });
 
-const loader = require(path.join(process.cwd(),'util','loader'));
+function loader(path, async){
+    const handler = filehound.create()
+    .path(path)
+    .ext([...Object.keys(require.extensions), '.coffee']);
+
+    if(async){
+        return new Promise((res,rej) => handler.find((err, files) => err ? rej(err) : res(files)));
+    }else{
+        return handler.findSync();
+    }
+}
+
 client.loader = loader;
 const handlers = loader(path.join(process.cwd(),'handlers'));
 
-const maxLength = handlers.reduce((p,c) => c.length > p ? c.length : p, 0);
+const maxLength = handlers.reduce((p,c) => Math.max(c.length, p), 0);
 const signLength = 15;
 
-for(const handler of handlers){
-    try{
-        require(handler)(client);
-        console.log(`${'HANDLER LOADED'.padEnd(signLength).green}${handler.padStart(maxLength).cyan}`);
-    }catch(err){
-        console.error(`${'HANDLER ERROR'.padEnd(signLength).red}${handler.padStart(maxLength).cyan}`);
-        console.error(err)
+(async function(){
+    for(const handler of handlers){
+        try{
+            await require(handler)(client);
+            console.log(`${'HANDLER LOADED'.padEnd(signLength).green}${handler.padStart(maxLength).cyan}`);
+        }catch(err){
+            console.error(`${'HANDLER ERROR'.padEnd(signLength).red}${handler.padStart(maxLength).cyan}`);
+            console.error(err);
+        }
     }
-}
+})();
