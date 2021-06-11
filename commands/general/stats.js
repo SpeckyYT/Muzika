96e88b2394e91870de9cb30c8745757cb824e37f
SpeckyYT <Specky.fans@gmail.com>
2021-06-11 22:33:34 +0200

    added "simple" stats command

---
+++ commands/general/stats.js
@@ -0,0 +1,37 @@
+const { Util: { escapeCodeBlock } } = require('discord.js');
+const pbs = require('pretty-bytes');
+const pms = require('pretty-ms');
+const os = require('os');
+const osu = require('node-os-utils');
+
+module.exports = {
+    trigger: [
+        'stats',
+        'statistics',
+    ],
+    async call(client, msg, ctx){
+        const username = client.user.username;
+        const promises = [
+            `🕹️ Discord Servers: ${client.guilds.cache.size}`,
+            `💿 Commands: ${client.commands.size}`,
+            `🎵 ${username} Size: ${pbs(process.memoryUsage().heapUsed)}`,
+            "",
+            `⛏️ CPUs: ${osu.cpu.count()} \`${escapeCodeBlock(osu.cpu.model())}\``,
+            osu.cpu.free().then(free => `🔧 CPU Usage: ${bar(100-free,100)}`),
+            osu.mem.free().then(f => `💾 RAM Usage: ${bar(f.totalMemMb-f.freeMemMb,f.totalMemMb)}`),
+            "",
+            `🤖 ${username} Uptime: ${pms(client.uptime)}`,
+            `📠 Process Uptime: ${pms(client.uptime)}`,
+            `🖥️ Device Uptime: ${pms(os.uptime() * 1000)}`,
+        ];
+        const strings = await Promise.all(promises);
+        return msg.reply(client.embed(strings.join('\n')));
+    }
+}
+
+function bar(value,max){
+    const size = 16;
+    const progress = Math.round((value / max) * size);
+    const remaining = size - progress;
+    return `${'█'.repeat(progress)}${'░'.repeat(remaining)}`;
+}
