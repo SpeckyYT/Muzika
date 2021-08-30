c199c6333d69329017823e547d87013b01f72104
SpeckyYT <Specky.fans@gmail.com>
2021-08-30 20:37:17 +0200

    Revert "oopsie"
    This reverts commit 8fca0fba85c3f137bdc269790d70917664c4bb90.

--- commands/music/radio.js
+++ commands/music/radio.js
@@ -62,7 +62,7 @@ module.exports = {
     category: 'music',
     usage: '[radio name]',
     limits: {
-        isPlaying: null,
+        isPlaying: false,
     },
     async call(client, msg, ctx){
         if(!ctx.body) return embed(client);
